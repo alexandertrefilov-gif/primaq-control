c1ffe6731a28c63d22680478585d374cf86b1795
Alexander Trefilov <alexandertrefilov@Mac.fritz.box>
2026-06-14 17:09:33 +0200

    feat(pwa): add offline support and service worker

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+import withPWA from "@ducanh2912/next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true
 };
 
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+  register: true,
+  reloadOnOnline: true,
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  disable: process.env.NODE_ENV === "development"
+})(nextConfig);
